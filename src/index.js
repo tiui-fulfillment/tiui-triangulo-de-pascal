58b1240967c8e25a00d669fe77dfe51dd857104d
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-27 17:27:48 -0600

    feat: Implement solution for pascal's triangle

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
  * @param {number} lineNumber - zero based.
  * @return {number[]}
  */
-
 const pascalTriangle = (lineNumber) => {
-  
-}
+  const row = [1];
+
+  for (let k = 1; k <= lineNumber; k++) {
+    row[k] = (row[k - 1] * (lineNumber - k + 1)) / k;
+  }
+
+  return row;
+};
 
 module.exports = pascalTriangle;
